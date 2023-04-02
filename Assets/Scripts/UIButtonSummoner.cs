c900243b93961f0ee14ba4ee0527eb00b94bf83b
Eric Gan <2019egan@gmail.com>
2023-04-02 16:36:41 -0400

    Linking UI to left hand motion

--- Assets/Scripts/UIButtonSummoner.cs
+++ Assets/Scripts/UIButtonSummoner.cs
@@ -6,9 +6,12 @@ public class UIButtonSummoner : MonoBehaviour
 {
     protected Animator[] children;
     public bool uiOut = false;
+    public float leftHandRotation = 0;
     // Start is called before the first frame update
     void Start()
     {
+        //0.6-1
+        leftHandRotation = transform.parent.transform.rotation.z;
         children = GetComponentsInChildren<Animator>();
         for (int i = 0; i < children.Length; i++) {
             children[i].SetBool("Out", uiOut);     
@@ -18,10 +21,20 @@ public class UIButtonSummoner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M)){
-            uiOut = !uiOut;
+        leftHandRotation = transform.parent.transform.rotation.z;
+        Debug.Log(transform.parent.transform.rotation.z);
+        if (leftHandRotation >= 0.6 && leftHandRotation <= 1){
+            if (uiOut) return;
+            uiOut = true;
             for (int i = 0; i < children.Length; i++) {
-                children[i].SetBool("Out", uiOut);     
+                children[i].SetBool("Out", true);     
+            }
+        } 
+        else {
+            if (!uiOut) return;
+            uiOut = false;
+            for (int i = 0; i < children.Length; i++) {
+                children[i].SetBool("Out", false);     
             }
         }
     }
